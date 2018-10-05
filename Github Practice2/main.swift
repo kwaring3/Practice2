d27a4262757e54f0d9abcc16c230a4df1a5db2b2
kwaring3 <kevinwaring@pursuit.org>
2018-10-05 15:31:47 -0400

    Initial Commit

---
+++ Github Practice2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Github Practice2
+//
+//  Created by Kevin Waring on 10/5/18.
+//  Copyright © 2018 Kevin Waring. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
